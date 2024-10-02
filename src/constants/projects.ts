f1bae780373c60a120842dab0b0f27adebccc8db
Alex Malko <Alex.Malko@madnesspartners.com>
2024-10-02 16:46:25 +0200

    added 4k view

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -6,7 +6,7 @@ import { SectionType } from '../enums/SectionType';
 export const PROJECTS: Project[] = [
   {
     id: "a79a22a4-5d13-448b-9f71-585146b6a368",
-    previewType: PreviewTemplateId.FULL_M_IL_TR,
+    previewType: PreviewTemplateId.FULL_S_IL_TR,
     sections: [
       {
         templateId: SectionTemplateId.FULL_S_IL_TR,
@@ -37,30 +37,30 @@ export const PROJECTS: Project[] = [
   },
   {
     id: "613e6f66-34be-468f-9da4-4ee387aaf7e4",
-    previewType: PreviewTemplateId.FULL_S_IL_TR,
+    previewType: PreviewTemplateId.FULL_XL_IL_TB,
     sections: [],
     isShownInMainPreviews: true,
   },
   {
     id: "cac17a19-de9c-43a7-903e-75319560726d",
-    previewType: PreviewTemplateId.FULL_XL_IC_TB,
+    previewType: PreviewTemplateId.FULL_L_IR_TB,
     sections: [],
     isShownInMainPreviews: true,
   },
   {
     id: "38178337-c403-49bd-b848-316eee8a4727",
-    previewType: PreviewTemplateId.FULL_S_IR_TL,
+    previewType: PreviewTemplateId.FULL_XL_IR_TB,
     sections: [],
     isShownInMainPreviews: true,
   },
   {
     id: "6824cc6b-ef17-45a5-be24-b9547312b8ac",
-    previewType: PreviewTemplateId.FULL_XL_IC_TB,
+    previewType: PreviewTemplateId.FULL_L_IL_TR,
     sections: [],
   },
   {
     id: "7fe36b44-a1af-446b-abe2-d2ad2ccef992",
-    previewType: PreviewTemplateId.FULL_XXL_IC_TB,
+    previewType: PreviewTemplateId.FULL_L_IL_TR,
     sections: [],
   },
   {
@@ -70,7 +70,7 @@ export const PROJECTS: Project[] = [
   },
   {
     id: "f8550e11-c724-4a45-9335-3a70e069aab6",
-    previewType: PreviewTemplateId.FULL_M_IL_TB,
+    previewType: PreviewTemplateId.FULL_L_IL_TR,
     sections: [],
   },
 ];
